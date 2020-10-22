81ce50d1a3b79170171547712143ca8f3cebd41a
Ivan Ivanov <Ivanivanov.ii726@gmail.com>
2020-10-22 10:57:51 +0300

    initial commit

---
+++ src/main/java/com/distance_learning/DistanceLearningApplication.java
@@ -0,0 +1,13 @@
+package com.distance_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanceLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistanceLearningApplication.class, args);
+    }
+
+}
